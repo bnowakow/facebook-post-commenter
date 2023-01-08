e49f8d1c7386f09bd7ffe16d63565f3a800ce26b
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-01-08 16:03:29 +0100

    add posts comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,22 @@
+import facebook4j.Comment
 import facebook4j.Facebook
 import facebook4j.FacebookFactory
+import facebook4j.Post
+import facebook4j.ResponseList
 
 fun main(args: Array<String>) {
 
     val facebook: Facebook = FacebookFactory().getInstance()
-    val posts = facebook.getPosts("105161449087504")
+    val posts: ResponseList<Post> = facebook.getPosts("105161449087504")
 
-    println("Posts: ${posts.joinToString()}")
+    for (post in posts) {
+        val comments: ResponseList<Comment> = facebook.getPostComments(post.id)
+
+        for (comment in comments) {
+            println("comment: ${comment}")
+        }
+    }
+
+
+
 }
