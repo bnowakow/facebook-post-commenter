95530db56e86f25ac62b373e840c5d9e1b8c81a8
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-01-09 19:17:28 +0100

    added limit until workaround not working paging in facebook4j

--- src/main/kotlin/FacebookReplies.kt
+++ src/main/kotlin/FacebookReplies.kt
@@ -1,20 +1,21 @@
-import facebook4j.Comment
-import facebook4j.Facebook
-import facebook4j.ResponseList
-import java.lang.StringBuilder
+import facebook4j.*
+
 
 class FacebookReplies(private val facebook: Facebook) {
 
+    val commentedPosts: Int = 0
+
     fun isCommentWrittenByOneOfAdmins(comment: Comment): Boolean {
-        return comment.from !== null && // non-Page comments are null
-            comment.from.id in arrayOf("105161449087504"); // Kuba
+        return comment?.from?.id == "105161449087504"; // Kuba
     }
 
     fun isCommentRepliedByOneOfAdmins(comment: Comment): Boolean {
 
         val commentsOfComment: ResponseList<Comment> = facebook.getCommentReplies(comment.id)
 
+        println("\thas comment ${comment.message}")
         for (commentOfComment in commentsOfComment) {
+            println("\t\twhich is commented by ${commentOfComment.from?.name}: ${commentOfComment.message}")
             if (isCommentWrittenByOneOfAdmins(commentOfComment)) {
                 return true;
             }
@@ -37,5 +38,40 @@ class FacebookReplies(private val facebook: Facebook) {
         reply.append(listOf("\n", "\n\n", " ").random())
         reply.append("Link do mojej zbiórki: http://siepomaga.pl/raczka-kuby")
         return reply.toString()
+    }
+
+    fun checkIfAllCommentsContainAdminComment(comments: PagableList<Comment>) {
+        for (comment in comments) {
+
+            if (!isCommentWrittenByOneOfAdmins(comment)) {
+                if (!isCommentRepliedByOneOfAdmins(comment)) {
+//                    println("\t\t\tcomment ${comment}");
+//                    println("comment ${comment} under post ${post.id}");
+
+                    facebook.likePost(comment.id)
+                    val replyMessage: String = randomizeThankYouReply()
+                    println("\t\t\ttrying replying with '${replyMessage}'")
+                    facebook.commentPost(comment.id, replyMessage)
+                    commentedPosts.inc()
+
+                    val numberOfSeconds: Long = (10..120).random().toLong()
+                    println("\t\t\tsleeping for ${numberOfSeconds} seconds\n")
+                    Thread.sleep(1000 * numberOfSeconds)
+                }
+            }
+        }
+    }
+
+    fun checkIfAllCommentsUnderPostContainAdminComment(post: Post) {
+        var comments: PagableList<Comment> = facebook.getPostComments(post.id, Reading().limit(250))
+        var paging: Paging<Comment>
+        println("in post [${post.message?.substring(0, 30)}...] got ${comments.size} comments on first page")
+
+        checkIfAllCommentsContainAdminComment(comments)
+
+//        while (comments.paging?.next !== null) {
+//            var url: URL = comments.paging.next
+//            comments = facebook.fetchNext(comments.paging)
+//        }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import facebook4j.*
+import java.net.URL
 import java.util.*
 
 
@@ -10,34 +11,12 @@ fun main(args: Array<String>) {
     val posts: ResponseList<Post> = facebook.getPosts("105161449087504") // Kuba
     println("got ${posts.size} posts")
 
-    val commentedPosts: Int = 0
-
     // comments
     for (post in posts) {
-        val comments: ResponseList<Comment> = facebook.getPostComments(post.id)
-        println("in post [${post.message?.substring(0, 30)}...] got ${comments.size} comments")
-
-        for (comment in comments) {
-
-            if (!facebookReplies.isCommentWrittenByOneOfAdmins(comment)) {
-                if (!facebookReplies.isCommentRepliedByOneOfAdmins(comment)) {
-                    println("comment ${comment} under post ${post.id}");
-
-                    facebook.likePost(comment.id)
-                    val replyMessage: String = facebookReplies.randomizeThankYouReply()
-                    println("\ttrying replying with '${replyMessage}'")
-                    facebook.commentPost(comment.id, replyMessage)
-                    commentedPosts.inc()
-
-                    val numberOfSeconds: Long = (10..120).random().toLong()
-                    println("sleeping for ${numberOfSeconds} seconds\n")
-                    Thread.sleep(1000 * numberOfSeconds)
-                }
-            }
-        }
+        facebookReplies.checkIfAllCommentsUnderPostContainAdminComment(post)
     }
 
-    println("added comment to ${commentedPosts.toString()} comments")
+    println("added comment to ${facebookReplies.commentedPosts.toString()} comments")
 
     // shared posts
 //    for (post in posts) {
