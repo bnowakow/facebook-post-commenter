59a28268ddeec8a5efbf178fca2a0b2706c327c9
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-01-13 16:24:55 +0100

    small refactor in main bringing api and workaround in foreach together

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,33 +24,42 @@ fun main(args: Array<String>) {
         facebookSharedPosts.switchProfileToFanPage()
     }
 
-    // shared posts
     for (post in posts) {
-//        val sharedPosts = facebook.getSharedPosts(post.id) // API
-        if (facebookProperties.getProperty("workaround-enabled") == "true" &&
-            facebookSharedPosts !== null) {
-            facebookSharedPosts.openSharedPosts(post.id) // Workaround
-        }
-    }
-
-    // comments
-    for (post in posts) {
+        // comments under posts via API
         if (facebook4jProperties.getProperty("enabled") == "true") {
             facebookReplies.checkIfAllCommentsUnderPostContainAdminComment(post)
+        }
+
+        // shared posts via API
+        //  val sharedPosts = facebook.getSharedPosts(post.id) // API
+
+        // shared posts using workaround
+        if (facebookProperties.getProperty("workaround-enabled") == "true" &&
+            facebookSharedPosts !== null) {
+            facebookSharedPosts.openSharedPosts(post.id)
         }
     }
 
     // TODO figure out can we get id's of ad posts from API
     val adPosts: List<String> = listOf(
-        "pfbid02Qnx3ctSvN2Z2JJDEzp25kcdsLgNVSNtHV1bF57psQTR5zWHY6NgEExRnSxMBw6A9l",
-        "pfbid0gywSSeZKvCFomR5dELyr2ULFpk35SLHAaE5USdiMeyWw4H6bi5yLBVrHnnVN4tuEl"
+        "105161449087504_pfbid02Qnx3ctSvN2Z2JJDEzp25kcdsLgNVSNtHV1bF57psQTR5zWHY6NgEExRnSxMBw6A9l",
+        "105161449087504_pfbid0gywSSeZKvCFomR5dELyr2ULFpk35SLHAaE5USdiMeyWw4H6bi5yLBVrHnnVN4tuEl"
     )
+
     for (adPost in adPosts) {
+        // comments under ad posts via API
         if (facebook4jProperties.getProperty("enabled") == "true") {
             facebookReplies.checkIfAllCommentsUnderPostContainAdminComment(
-                facebook.getPost("105161449087504_" + adPost)
+                facebook.getPost(adPost)
             )
         }
+
+        // shared ad posts using workaround
+        // TODO check how to get shared posts of ads
+//        if (facebookProperties.getProperty("workaround-enabled") == "true" &&
+//            facebookSharedPosts !== null) {
+//            facebookSharedPosts.openSharedPosts(adPost)
+//        }
     }
 
     logger.info("added comment to ${facebookReplies.commentedPosts.toString()} comments")
