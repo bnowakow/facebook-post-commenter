7370d2e812776f7b4248b91715bb4a19e99f605c
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-04-11 21:35:26 +0200

    wip

--- loop.sh
+++ loop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# for error "network bridge not found" do sudo ~/code/bash_configs/nas/change-iptables-bridge-docker-settings.sh 
+# TODO check if build was successfull
 docker-compose build
 
 while true; do
