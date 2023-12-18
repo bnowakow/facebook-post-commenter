3404b14e7fb470c6a8cffa6051336d3e7738af07
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2023-12-18 14:44:51 +0100

    remove ad posts duplicates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -49,6 +49,7 @@ fun main() {
 //        "105161449087504_pfbid0owVM3rT1Bkww4QjdTLDbmZz6ZcCx5KskkvGbuR15q2pq3PS5sJwTuTKCj8AN7Npvl",        // duplicate of 7
 //        "105161449087504_pfbid02snw4EXNq3AgYv72ai7NwxJocbsfCjGz3wwzzk9xq4ErRuP7BUQ1sNAfGT93r2MYdl",       // duplicate of 7
 //        "105161449087504_pfbid02scn3wP76DSTLLaqSntN3MBTnNS7PDFbZcDEfh94eRPj9agx1nhcWSuiQ3P3kCp5Yl",       // duplicate of 7
+//        "105161449087504_pfbid02spR7V5rXSe7snXKF3iMhDkQPsb2fLKzhE3waenpzyAPFkEQJAvNWz6PEF2Eqc2zHl",       // duplicate of 7
         "105161449087504_pfbid02PmtVH17t6Vcu811B4KvrBodB4ZdJ5PhwD9VdsrF69kpakZnaPeKzp4WRqUTR3q92l",         // 8
 //        "105161449087504_pfbid02PbjUyrr9GtXAvVnDzvYJ26s41eBpqfnn3mFcdPNWBdkZcMPDPH6F2vU5anmwYJEBl",       // duplicate of 8
 //        "105161449087504_pfbid0L46J4hDnRkHUVkzn2ZUoatFFfM8KE63SJegAkXv84UDiz9d6fKHH7P2XniENTSN8l",        // duplicate of 8
@@ -56,6 +57,7 @@ fun main() {
 //        "105161449087504_pfbid0KswHmYx3c4tWxmKjSPNu6yi9q9VDaceFNsWCxHT4NTiCaQATMhT8BjzSbB8C9Jgjl",        // duplicate of 8
 //        "105161449087504_pfbid02PjNzxDssTZMT97qidc73xFeF2R5zTPaRFSbwnLg87q6LWHTrSZWYKJkoBMQckREil",       // duplicate of 8
 //        "105161449087504_pfbid02PZDzf5c8e3hSt4PvDgPxRatxukGQyCu5ZzHCqz8Gyz6FFfwkeeqwFsuTJYLnUeyBl",       // duplicate of 8
+//        "105161449087504_pfbid02Pfj2ZCZrKPc1jGNK6drSM33n3DGDwUiLV67o1XBe8JWaAshZXEXddWvEWohY4b6vl",       // duplicate of 8
         "105161449087504_pfbid0jDpKaCbboTVsmT7yKWDMsPcsGbrEkMHJGUD2CyUVv31QcbdhZv9HiZsoBJJGJAVtl",          // 9
 //        "105161449087504_pfbid02o5G2ksXResxMN3cjBD7xubYiMPTYWfjrRSmxUYaoPspbApvUi1X1zHPcDHkq5xinl",       // duplicate of 9
 //        "105161449087504_pfbid0j3fKH4Krz4UMJMD5cM3DrvZbBtAqddCUANf4AnJfsVEU9BKKRVvzyrXh7oKvakxrl",        // duplicate of 9
@@ -64,6 +66,7 @@ fun main() {
 //        "105161449087504_pfbid02niqgni5wHfEdrTm9696tFFYA8x7GZwcSa1Bfd4RSquaA1XZ9TpGDD1V3CKJ8S1KDl",       // duplicate of 9
 //        "105161449087504_pfbid0jBCVsYTaSQRdM5KtigdFSBwGkZ7EeZ3DevzAAFy42fHzhSu79fBcciC3GRFoCUpNl",        // duplicate of 9
 //        "105161449087504_pfbid02o2eD4DPQHtio1Gja2cyQ7as7JrZeZrtE8SC1yPa8Ly4C1nSuF5ctUJzzQgLLKAdPl",       // duplicate of 9
+//        "105161449087504_pfbid02nsxYPVBgLAXRjYPT1KyFkeHrjbqE7SEopugAtPt8A76BeT4qhkLdD3kc23wunRail",       // duplicate of 9
         "105161449087504_pfbid036aUsfYo8NpiZR5MhSyLcB16HuxxCG5QrpRWJStWBkNgSQKmjfRJ9E3hG62Pr94Hsl",         // 10
 //        "105161449087504_pfbid0232qgkPAmXVF1dNoFCa4VuqWMKqY5f2qRm4GPeirhGP6PrFDkwkXJZ9xuGqA9dZ2kl",       // duplicate of 10
 //        "105161449087504_pfbid036tHPw46bNrcPH7WxeWQ473QLSwioMw1N8TaRp6A8pU9b7TJSmc8KGpTKEReEn7rdl",       // duplicate of 10
@@ -71,6 +74,7 @@ fun main() {
 //        "105161449087504_pfbid036i8PduprZUuVGrpJ2Jn3ohh1A8knEpWCViwGoKMHGZLYpSzWM94hC9cDLudX9ddel",       // duplicate of 10
 //        "105161449087504_pfbid023AVCikCViHdR1iPR8usMySacGHr5kke2on8M4d8U7naB9co7iKxqMz7EoubpASf3l",       // duplicate of 10
 //        "105161449087504_pfbid036XyPLmZ7jnHxNNXiBXckT82gC9JieF9E2eho9aRUFtWNfHbK237nNnSpq548ELyul",       // duplicate of 10
+//        "105161449087504_pfbid0234K2NoEnPNGgXTNbXRGkAgBgEqTxNkBuFb6AtHbL4FVvyF8aVqJFE6QRaz16Lu9el",       // duplicate of 10
         "105161449087504_pfbid02JgVMb7AhbxAgLzb7YgEMVr9A8eTz5h5ytepAVPeNSZTkctLk84zycGnSeWfa212kl",         // 11
 //        "105161449087504_pfbid0F8rAfwYLkdEhTZzhnKwzS5CgAoxBigJfyFoxeqtfDZSePyMBQESCrmN5rMcydz8fl",        // duplicate of 11
 //        "105161449087504_pfbid02JzHsrcUAc1sFPynMyYYvE6a1mR1LeMPhshNRWHsix3bt6Shc6G1dg8W62VQ8tdkil",       // duplicate of 11
@@ -79,30 +83,25 @@ fun main() {
 //        "105161449087504_pfbid02K7wPpyVtnkPQYYnBdBx9NceZK48C6ZdCezC5BF7uv8LHdP1oyaH4Z9DwqXCVXPdSl",       // duplicate of 11
 //        "105161449087504_pfbid0FQ9Ccfbo83xQSnAJR4brUsFqPacZ9tTot92fShrf8MMkyKteb61pzMQk3mDrjvEhl",        // duplicate of 11
 //        "105161449087504_pfbid02JmdPEgxR9dD2V4HK5hLQkWRBC4PN8tmFc8ieaSkuBdECF3qnWNcoFGUpzzL4vjbvl",       // duplicate of 11
+//        "105161449087504_pfbid0FNyzvH874wZ4K2QadSCnwrarqnbTAdGvV2vSCUtHWzvUigNjhFE6vjwiBcZig2aGl",        // duplicate of 11
         "105161449087504_pfbid02i3af3dUCAKpyNX8WVYW9HSyme3QfsQ4gFvfBK7apFPW2bZUdNPBjwcSb4tLEKhZTl",         // 12
+//        "105161449087504_pfbid02iHbSHwkGCTPBSyz78q1BcmPr9YgGg1i2SenyRD7w8rqx2atKjsWDfZ3fg4LLnFfTl",       // duplicate of 12
         "105161449087504_pfbid02hyeu3vLQyL5uPy7YDF8HWpAsJggibkQ1BhZUHDJEABANTdypHSskUq25mrNSpCiFl",         // 13
+//        "105161449087504_pfbid02hxNeMWZ2eNgyokcBKcXnee781svAPbzSxedDuqzPD5V4V3ctbCEuPnmoyD8TsVJHl",       // duplicate of 13
         "105161449087504_pfbid01TEkQjff5D7Wg7BgaPPQcFVoonErBHVAPC4HYAfYTmboxd3zid5x9nKCwxZ5cJtBl",          // 14
+//        "105161449087504_pfbid024qxAf8UuQmNPBcq2mcaek7itGgZr5fNxmdcYtxUS6eZtydvnytbsQTVErcVxg63Rl",       // duplicate of 14
         "105161449087504_pfbid028TLQmc2eTrxkbrWbNvHcSXRiCzidtk1AbW6zESN8KTwwrheDv5XRuWgQpFyudj7Ql",         // 15
         "105161449087504_pfbid02T7cDoH6JSo1nsNhAN5DxBkrt1zWWCXGYbhrFTiJAiZskW9PWMjA97vptFNjMSN3nl",         // 16
         "105161449087504_pfbid0zmkxMxMGZ6LD89PJfAzWHPaYBfgW9wzsV4txipv4a1f4FFHcJBupbS3eNUTfHmbil",          // 17
-
-        "105161449087504_pfbid02spR7V5rXSe7snXKF3iMhDkQPsb2fLKzhE3waenpzyAPFkEQJAvNWz6PEF2Eqc2zHl",         // TODO check duplicates
-        "105161449087504_pfbid02Pfj2ZCZrKPc1jGNK6drSM33n3DGDwUiLV67o1XBe8JWaAshZXEXddWvEWohY4b6vl",         // TODO check duplicates
-        "105161449087504_pfbid02nsxYPVBgLAXRjYPT1KyFkeHrjbqE7SEopugAtPt8A76BeT4qhkLdD3kc23wunRail",         // TODO check duplicates
-        "105161449087504_pfbid0234K2NoEnPNGgXTNbXRGkAgBgEqTxNkBuFb6AtHbL4FVvyF8aVqJFE6QRaz16Lu9el",         // TODO check duplicates
-        "105161449087504_pfbid0FNyzvH874wZ4K2QadSCnwrarqnbTAdGvV2vSCUtHWzvUigNjhFE6vjwiBcZig2aGl",          // TODO check duplicates
-        "105161449087504_pfbid02iHbSHwkGCTPBSyz78q1BcmPr9YgGg1i2SenyRD7w8rqx2atKjsWDfZ3fg4LLnFfTl",         // TODO check duplicates
-        "105161449087504_pfbid02hxNeMWZ2eNgyokcBKcXnee781svAPbzSxedDuqzPD5V4V3ctbCEuPnmoyD8TsVJHl",         // TODO check duplicates
-        "105161449087504_pfbid024qxAf8UuQmNPBcq2mcaek7itGgZr5fNxmdcYtxUS6eZtydvnytbsQTVErcVxg63Rl",         // TODO check duplicates
-        "105161449087504_pfbid0zhMxLeoQD6EYGJ1o8mEn7ZyBYrA9pU22Ddx9rfck5FbLUyKZnmKM4sHzEvuVhKzRl",          // TODO check duplicates
-        "105161449087504_pfbid02WgCsMXh1iHpcXZV4hRRze4VbH9o6m1WF3GhALdwV3xGLGoPMVzQesbcq4GZ4dnEKl",         // TODO check duplicates
-        "105161449087504_pfbid02ZEWhCkcxSDjtyhmYUm5kWMU8tJYVEWSGpAkCJPX4ywBq2dRzgsK96XnDvGNzMD2ul",         // TODO check duplicates
-        "105161449087504_pfbid0324KVCiwC9m9CTfwEWRZ5gA8fVTsCNPzLNSkKc78PCy8wgNHKvtTSMVy3o6u7S7cFl",         // TODO check duplicates
-        "105161449087504_pfbid0uL1Bxr3wRuc8Z1TBRWzbXuHYuBAeK9bUDf5rEAqQoDsz8nZr8SuKFPNveqNuCV63l",          // TODO check duplicates
-        "105161449087504_pfbid02MmXqJi3FbVzH6fwsN38QYd4yC9onmHbLtHeXt6bE3q1NmvR3sEBhDho9D6A1gmPXl",         // TODO check duplicates
-        "105161449087504_pfbid02iz7SkNPkTsTRKbMWywENF7bHpjv6GXr8hjYVaLuyY1Up99YHXnvr6Zd5xYobNo4Cl",         // TODO check duplicates
-        "105161449087504_pfbid02b2v9jGWQ7uNJ8LNgXDNXBhTXY2gw5N1C6NuHm7seHdNitNTAPKTcmRpG5Dexsndxl",         // TODO check duplicates
-        "105161449087504_pfbid025gDuHwiPb3J3whCpfQME5nfeHRYkTvPB4ZPk8N3M74H6CG9HYQJo2898kcJB5Wmyl",         // TODO check duplicates
+//        "105161449087504_pfbid0zhMxLeoQD6EYGJ1o8mEn7ZyBYrA9pU22Ddx9rfck5FbLUyKZnmKM4sHzEvuVhKzRl",        // duplicate of 17
+        "105161449087504_pfbid02WgCsMXh1iHpcXZV4hRRze4VbH9o6m1WF3GhALdwV3xGLGoPMVzQesbcq4GZ4dnEKl",         // 18
+        "105161449087504_pfbid02ZEWhCkcxSDjtyhmYUm5kWMU8tJYVEWSGpAkCJPX4ywBq2dRzgsK96XnDvGNzMD2ul",         // 19
+        "105161449087504_pfbid0324KVCiwC9m9CTfwEWRZ5gA8fVTsCNPzLNSkKc78PCy8wgNHKvtTSMVy3o6u7S7cFl",         // 20
+        "105161449087504_pfbid0uL1Bxr3wRuc8Z1TBRWzbXuHYuBAeK9bUDf5rEAqQoDsz8nZr8SuKFPNveqNuCV63l",          // 21
+        "105161449087504_pfbid02MmXqJi3FbVzH6fwsN38QYd4yC9onmHbLtHeXt6bE3q1NmvR3sEBhDho9D6A1gmPXl",         // 22
+        "105161449087504_pfbid02iz7SkNPkTsTRKbMWywENF7bHpjv6GXr8hjYVaLuyY1Up99YHXnvr6Zd5xYobNo4Cl",         // 23
+        "105161449087504_pfbid02b2v9jGWQ7uNJ8LNgXDNXBhTXY2gw5N1C6NuHm7seHdNitNTAPKTcmRpG5Dexsndxl",         // 24
+        "105161449087504_pfbid025gDuHwiPb3J3whCpfQME5nfeHRYkTvPB4ZPk8N3M74H6CG9HYQJo2898kcJB5Wmyl",         // 25
 
         // todo: 781862799854716 https://www.facebook.com/Kuba.Dobrowolski.Nowakowski/videos/781862799854716
         // todo: https://www.facebook.com/Kuba.Dobrowolski.Nowakowski/videos/777488547419139/
