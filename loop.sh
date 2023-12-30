ff3ca5cb115bb0c2fa2cf8829093c5fac93b29ff
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2023-12-30 02:26:40 -0800

    extend timeout and git pull in loop.sh

--- loop.sh
+++ loop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+git pull
 # for error "network bridge not found" do sudo ~/code/bash_configs/nas/change-iptables-bridge-docker-settings.sh 
 # TODO check if build was successfull
 docker compose build
 
 while true; do
-    timeout 1800 docker compose up
+    timeout 3600 docker compose up
     docker compose down
     date
     sleep 6h
