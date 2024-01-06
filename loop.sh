914720e7f0b16e8eecce6c4298ab68c04ed9888e
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-01-06 22:27:16 +0100

    workaround for failed docker compose build on nas

--- loop.sh
+++ loop.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 git pull
+
 # for error "network bridge not found" do sudo ~/code/bash_configs/nas/change-iptables-bridge-docker-settings.sh 
 # TODO check if build was successfull
-docker compose build
+#docker compose build
+# above fails on nas so as workadound we're building below on laptop
+# https://www.docker.com/blog/multi-arch-build-and-images-the-simple-way/
+# docker buildx create --use
+#docker buildx build --push --platform linux/amd64  --tag bnowakow/facebook-post-commenter:latest .
+docker buildx build --platform linux/amd64  --tag bnowakow/facebook-post-commenter:latest .
 
 while true; do
     timeout 3600 docker compose up
