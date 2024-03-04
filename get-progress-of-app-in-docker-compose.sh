87344b45ea3cf47dffc0fc167e32624ce4075b24
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-03-03 23:55:37 -0800

    update get-progress-of-app-in-docker-compose.sh

--- get-progress-of-app-in-docker-compose.sh
+++ get-progress-of-app-in-docker-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker compose logs -f | grep -P 'already posted this. Posting the same content repeatedly on Facebook isn|added comment to .* comments via|in .* post \['
-
+# TODO for some reason "couldn.* th user on invite to fan page screen" doesn't work
+docker compose logs -f | grep -P 'already posted this. Posting the same content repeatedly on Facebook isn|added comment to .* comments via|in .* post \[|fail to add comment to .* post|couldn.* th user on invite to fan page screen'
 
