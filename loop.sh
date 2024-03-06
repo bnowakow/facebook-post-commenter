42540e70f18cda556132f1ba52ee5f1960a2a2d4
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-03-05 22:12:23 -0800

    mount adPosts.txt to inside of container so it could be updated from it. build docker on each loop cycle so docker container could be updated after updates in adPosts.txt

--- loop.sh
+++ loop.sh
@@ -4,15 +4,15 @@ git pull
 
 # for error "network bridge not found" do sudo ~/code/bash_configs/nas/change-iptables-bridge-docker-settings.sh 
 # TODO check if build was successfull
-#docker compose build
+# docker compose build
 # above fails on nas so as workadound we're building below on laptop
 # https://www.docker.com/blog/multi-arch-build-and-images-the-simple-way/
 # docker buildx create --use
-#docker buildx build --push --platform linux/amd64  --tag bnowakow/facebook-post-commenter:latest .
-docker buildx build --platform linux/amd64  --tag bnowakow/facebook-post-commenter:latest .
-yes | docker system prune
+# docker buildx build --push --platform linux/amd64  --tag bnowakow/facebook-post-commenter:latest .
 
 while true; do
+    docker buildx build --platform linux/amd64  --tag bnowakow/facebook-post-commenter:latest .
+    yes | docker system prune
     time timeout 10h docker compose up
     docker compose down
     date
